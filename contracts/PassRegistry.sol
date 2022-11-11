a1142ef77635acc5d426dc7a1ee3a189bc42e2cd
dom <dom@gmail.com>
2022-11-11 17:42:14 +0800

    update

--- contracts/PassRegistry.sol
+++ contracts/PassRegistry.sol
@@ -2,125 +2,195 @@
 // SPDX-License-Identifier: None
 pragma solidity >=0.8.4;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableSetUpgradeable.sol";
-import "./StringUtils.sol";
 
 contract PassRegistryStorage {
+    struct PassInfo {
+        uint passId;
+        bytes32 passClass;
+        bytes32 passHash;
+    }
     mapping(address => uint) userInvitedNum;
     mapping(address => uint) userInvitesMax;
     mapping(bytes32 => address) hashToOwner;
     mapping(bytes32 => string) hashToName;
+    mapping(uint => PassInfo) passInfo;
 }
 
-contract PassRegistry is PassRegistryStorage, ERC721Upgradeable {
-    using StringUtils for *;
+contract PassRegistry is PassRegistryStorage, ERC721EnumerableUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
-    using EnumerableSetUpgradeable for EnumerableSetUpgradeable.Bytes32Set;
-
-    enum UserClass {A, B, C }
 
     uint8 constant ClassAInvitationNum = 7;
     uint8 constant ClassBInvitationNum = 6;
     uint8 constant ClassCInvitationNum = 3;
-    bytes32 constant ClassA = "";
-    bytes32 constant ClassB = "";
-    bytes32 constant ClassC = "";
+    uint8 constant ClassANameLen = 2;
+    uint8 constant ClassBNameLen = 3;
+    uint8 constant ClassCNameLen = 6;
+    bytes32 constant ClassA = 0x03783fac2efed8fbc9ad443e592ee30e61d65f471140c10ca155e937b435b760; // A
+    bytes32 constant ClassB = 0x1f675bff07515f5df96737194ea945c36c41e7b4fcef307b7cd4d0e602a69111; // B
+    bytes32 constant ClassC = 0x017e667f4b8c174291d1543c466717566e206df1bfd6f30271055ddafdb18f72; //C
 
+    address public admin;
     CountersUpgradeable.Counter private passId;
-    mapping(address => EnumerableSetUpgradeable.Bytes32Set) private userNames;
 
-    function initialize(string memory _name, string memory _symbol) public initializer {
+    // EVENTs
+    event LockPass(address user, uint passNumber);
+    event LockName(address user, uint passId, string name);
+
+    function initialize(address _admin, string memory _name, string memory _symbol) public initializer {
+        admin = _admin;
         __ERC721_init(_name, _symbol);
     }
 
+    function getClassInfo(bytes32 _hash) public pure returns (uint invNum, uint nameLen, bytes32 class) {
+        invNum = ClassCInvitationNum;
+        nameLen = ClassCNameLen;
+        class = ClassC;
+        if(_hash == ClassA) {
+            invNum = ClassAInvitationNum;
+            nameLen = ClassANameLen;
+            class = ClassA;
+        }else if(_hash == ClassB){
+            invNum = ClassBInvitationNum;
+            nameLen = ClassANameLen;
+            class = ClassB;
+        }
+    }
 
     /**
-    * @notice lock a name by pass
+    * @notice lock a name with code
      */
-    function lockPass(bytes memory _invitationCode, string memory _name, string memory _msg) external {
-        //bytes32 hashedName = keccak256(abi.encodePacked(_name));
-        bytes32 hashedMsg = keccak256(abi.encodePacked(_msg));
+    function lockPass(bytes memory _invitationCode, string memory _name, string memory _class) external {
+        bytes32 hashedMsg = keccak256(abi.encodePacked(_class));
         address codeFrom = recoverSigner(hashedMsg, _invitationCode);
-        require(userInvitesMax[codeFrom] > userInvitedNum[codeFrom], "IC");
+        if(codeFrom != admin){
+            require(userInvitesMax[codeFrom] > userInvitedNum[codeFrom], "IC");
+        }
 
-        uint passNum = 0;
-        if (hashedMsg == ClassA){
-            passNum = ClassAInvitationNum;
-        }else if (hashedMsg == ClassB){
-            passNum = ClassBInvitationNum;
-        }else{
-            passNum = ClassCInvitationNum;
+        (uint passNum, uint nameLen, bytes32 class) = getClassInfo(hashedMsg);
+
+        uint lockingPassId = passId.current() + 1;
+
+        // mint passes
+        for (uint256 index = 0; index < passNum; index++) {
+            passId.increment();
+            passInfo[passId.current()] = PassInfo({
+                passId: passId.current(),
+                passClass: ClassC,
+                passHash: ""
+            });
+            _mint(msg.sender, passId.current());
         }
 
         // bind name if possible
-        if(nameAvaliable(_name)) {
-            userInvitedNum[codeFrom] += 1;
+        bytes32 hashedName = keccak256(abi.encodePacked(_name));
+        userInvitedNum[codeFrom] += 1;
+        passInfo[lockingPassId].passClass = class;
+        passInfo[lockingPassId].passHash = hashedName;
+        _lockName(lockingPassId, _name, hashedName, nameLen);
 
-            passId.increment();
-            _mint(msg.sender, passId.current());
-
-            lockName(passId.current(), _name);
-        }
-
-        // mint invitecodes
-        for (uint256 index = 0; index < passNum; index++) {
-            passId.increment();
-            _mint(msg.sender, passId.current());
-        }
+        emit LockPass(msg.sender, passNum);
     } 
 
     /**
     * @notice lock a name with given passid
      */
-    function lockName(uint _passId, string memory _name) public {
-        require(ownerOf(_passId) == msg.sender, "IP");
+    function lockName (uint _passId, string memory _name) external {
         bytes32 hashedName = keccak256(abi.encodePacked(_name));
-        require(hashToOwner[hashedName] != address(0), "IN");
+        PassInfo memory pass = passInfo[_passId];
+
+        (uint passNum, uint nameLen, bytes32 class) = getClassInfo(pass.passClass);
+
+        require(_lockName(_passId, _name, hashedName, nameLen));
+    }
+
+    function _lockName(uint _passId, string memory _name, bytes32 _hashedName, uint _minLen) internal returns (bool){
+        if(!nameAvaliable(_minLen, _name)){
+            return false;
+        }
+        require(ownerOf(_passId) == msg.sender, "IP");
+        require(hashToOwner[_hashedName] != address(0), "IN");
 
         //lock name
-        userNames[msg.sender].add(hashedName);
-        hashToName[hashedName] = _name;
-        hashToOwner[hashedName] = msg.sender;
-        // init invatations
-        userInvitedNum[msg.sender] = 0;
-        userInvitesMax[msg.sender] = 3;
+        hashToName[_hashedName] = _name;
+        hashToOwner[_hashedName] = msg.sender;
+        // init invatations at first time
+        if(userInvitesMax[msg.sender] == 0){
+            userInvitedNum[msg.sender] = 0;
+            userInvitesMax[msg.sender] = 3;
+        }
+
+        emit LockName(msg.sender, _passId, _name);
+        return true;
     }
 
     /**
     * @notice request user's pass list
      */
-    function getUserPassList(address _user) external view returns (string[] memory) {
-        string[] memory names = new string[](userNames[_user].length());
+    function getUserPassList(address _user) external view returns (uint[] memory) {
+        //string[] memory names = new string[](balanceOf(_user));
+        uint[] memory passList = new uint[](balanceOf(_user));
 
-        for (uint256 index = 0; index < userNames[_user].length(); index++) {
-            bytes32 hashedName = userNames[_user].at(index);
-            names[index] = hashToName[hashedName];
+        for (uint256 index = 0; index < balanceOf(_user); index++) {
+            //names[index] = hashToName[passIdToHash[tokenOfOwnerByIndex(_user, index)]];
+            passList[index] = tokenOfOwnerByIndex(_user, index);
         }
-        return names;
+        return passList;
     }
 
     /**
     * @notice check name length
     */
-    function lenValid(string memory _name) public pure returns (bool){
-        //return name.strlen() >= 2;
-        return true;
+    function lenValid(uint _minLen, string memory _name) public pure returns (bool){
+        return strlen(_name) >= _minLen;
     }
 
     /**
     * @notice check if name match some patterns
      */
-    function matchValid(string memory _name) internal pure returns (bool) {
+    function matchDenyList(string memory _name) internal pure returns (bool) {
+        return false;
+    }
+
+    function nameAvaliable(uint _minLen, string memory _name) public view returns (bool) {
+        if(lenValid(_minLen, _name)){
+
+        }
+        // is locked before
+        bytes32 nameHash = keccak256(bytes(_name));
+        if(hashToOwner[nameHash] == address(0)){
+            return false;
+        }
+        // name deny list
+        if(matchDenyList(_name)){
+            return false;
+        }
         return true;
     }
 
-    function nameAvaliable(string memory _name) public view returns (bool) {
-        bytes32 nameHash = keccak256(bytes(_name));
-        return matchValid(_name) && lenValid(_name) && hashToOwner[nameHash] == address(0);
+    function strlen(string memory s) internal pure returns (uint256) {
+        uint256 len;
+        uint256 i = 0;
+        uint256 bytelength = bytes(s).length;
+        for (len = 0; i < bytelength; len++) {
+            bytes1 b = bytes(s)[i];
+            if (b < 0x80) {
+                i += 1;
+            } else if (b < 0xE0) {
+                i += 2;
+            } else if (b < 0xF0) {
+                i += 3;
+            } else if (b < 0xF8) {
+                i += 4;
+            } else if (b < 0xFC) {
+                i += 5;
+            } else {
+                i += 6;
+            }
+        }
+        return len;
     }
-
 
     /**
     * @notice verify a request code by message and its signature
