84bf353353a9f4209c22513f7831d087c378cd6e
Aarav <116242667+AaravClayton@users.noreply.github.com>
2022-11-18 18:18:47 +0800

    reserve names and gas report

--- scripts/reserveNames.js
+++ scripts/reserveNames.js
@@ -1,4 +1,7 @@
 const hre = require('hardhat')
+const fs = require('fs')
+const readline = require('readline')
+var uts46 = require('idna-uts46-hx')
 
 // upgrade contract
 async function main() {
@@ -8,16 +11,34 @@ async function main() {
     Registry,
   )
 
-  let names = [
-
-  ]
+  let names = []
   let hashnames = []
-  names.forEach(element => {
-    hashnames.push(ethers.utils.keccak256(ethers.utils.toUtf8Bytes(element)))
-  });
+
+  const readStream = fs.createReadStream('scripts/reserveNames.txt')
+  const writeStream = fs.createWriteStream('scripts/reserveNames2.txt')
+
+  const rl = readline.createInterface({
+    input: readStream,
+    crlfDelay: Infinity,
+  })
+
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    let name = line.toLowerCase().replace(/[\s'&.,]+/g, '')
+    if (name.length == 0) continue
+    names.push(name)
+    hashnames.push(ethers.utils.keccak256(ethers.utils.toUtf8Bytes(name)))
+    writeStream.write(name)
+    writeStream.write('\n')
+    // console.log(name)
+    try {
+      uts46.toUnicode(name, { useStd3ASCII: true, transitional: false })
+    } catch (e) {
+      console.error(name, e)
+    }
+  }
 
   console.log(await proxy.estimateGas.reserveNames(hashnames))
-
 }
 
 main().catch((error) => {
