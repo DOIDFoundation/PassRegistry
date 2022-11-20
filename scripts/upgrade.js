99ceead6b5c2ff3e7d4c6d58759c2f4bdd874434
Aarav <116242667+AaravClayton@users.noreply.github.com>
2022-11-20 14:26:03 +0800

    meta image update

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -4,7 +4,8 @@ const hre = require('hardhat')
 async function main() {
   const Registry = await hre.ethers.getContractFactory('PassRegistry')
   const proxy = await upgrades.upgradeProxy(
-    '0x208ec0Ef36E94F582841296dcA6F6B61d5823fBE',
+    '0x208ec0Ef36E94F582841296dcA6F6B61d5823fBE', //Sepolia
+    // '0x9281fD776D35f518B877D726e6a30eff1c7775E9', //Goerli
     Registry,
   )
   console.log('upgrade proxy', proxy.address)
