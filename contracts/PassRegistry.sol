61975e6364436cc36c94814985309f7316844fa6
ac.doid <116242667+ac-doid@users.noreply.github.com>
2022-11-25 20:52:02 +0800

    fix invitation verification

--- contracts/PassRegistry.sol
+++ contracts/PassRegistry.sol
@@ -254,10 +254,10 @@ contract PassRegistry is
     }
 
     // for testing
-    // function deactivateUser(address _to) external {
-    //     _checkRole(DEFAULT_ADMIN_ROLE, 0xAFB2e1145f1a88CE489D22425AC84003Fe50b3BE);
-    //     delete userActivated[_to];
-    // }
+    function deactivateUser(address _to) external {
+        _checkRole(DEFAULT_ADMIN_ROLE, 0xAFB2e1145f1a88CE489D22425AC84003Fe50b3BE);
+        delete userActivated[_to];
+    }
 
     /**
      * @notice check if name has already been registered
@@ -327,7 +327,7 @@ contract PassRegistry is
      * @notice verify a request code by message and its signature
      */
     function verifyInvitationCode(bytes32 _msg, bytes memory _sig) public pure returns (address) {
-        if (_sig.length == 32) {
+        if (_msg == ClassC && _sig.length == 32) {
             bytes32 chunk;
             assembly {
                 chunk := mload(add(_sig, 32))
