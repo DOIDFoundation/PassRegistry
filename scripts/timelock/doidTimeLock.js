46efbf8b94a77d73862dd3b2575b35199682b773
dom <dom@example.com>
2024-02-28 19:02:31 +0800

    add scripts of timelock

--- scripts/timelock/doidTimeLock.js
+++ scripts/timelock/doidTimeLock.js
@@ -1,11 +1,26 @@
-
 const hre = require('hardhat')
-
 
 async function main() {
   const accounts = await hre.ethers.getSigners()
   admin = accounts[0]
   console.log('admin:', admin.address)
+
+  const testnet = ''
+  const timelock = await hre.ethers.getContractAt('DoidTimeLock', testnet)
+
+  // locking ETH
+  const unlockTimestamp = 1709096516
+  const lockAmount = 100
+  let tx = await timelock.queue(unlockTimestamp, { value: lockAmount })
+  console.log('debug tx', await tx.wait())
+
+  // find out my locking info
+  const info = await timelock.getQueue(admin.address)
+  console.log('my locking info:', info)
+
+  // unlock ETH after blocks
+  tx = await timelock.execute()
+  console.log(await tx.wait())
 }
 
 // We recommend this pattern to be able to use async/await everywhere
